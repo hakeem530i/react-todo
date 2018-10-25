f54daf1c52449d83728a13743f86496753841cc1
thechocolateman <hakeemjgreen@gmail.com>
2018-10-25 12:33:47 -0400

    Catch errors and update styling

--- src/App.js
+++ src/App.js
@@ -16,64 +16,66 @@ class App extends Component {
     //this.setState({ currentItem: event.target.value });
     console.log(this.state.currentItem);
   };
+
+  //Add Note button
+  addButton = () => {
+    alert("Clicked");
+  };
+
+  //Save button click
+  saveClick = () => {
+    var joined = this.state.notes.concat(this.state.currentItem);
+    console.log(this.state.currentItem);
+
+    //If field isn't empty
+    if (this.state.currentItem.length > 0) {
+      this.setState(
+        {
+          notes: joined
+        },
+        () => this.setState({ currentItem: "" })
+      );
+      console.log(`Current item is: ${this.state.currentItem.length}`);
+    } else {
+      alert("Enter text");
+    }
+  };
+  deleteNote = () => {
+    alert("Delete");
+  };
   render() {
     return (
       <main className="App">
         <div className="w-100">
           <div className="container center w-50">
-            <div className="w-100 flex">
+            <div className="w-70 tl center pa3">
               <h1 className="fl">To-Do List</h1>
-              <button
-                onClick={() => {
-                  alert("Clicked");
+            </div>
+            <div className="w-70 center">
+              <input
+                onChange={event => {
+                  this.setState({ currentItem: event.target.value });
                 }}
-              >
-                <span className="oi oi-plus fr" />
+                className="pa3 w-70"
+                placeholder="Type and press enter to save"
+                type="text"
+                value={this.state.currentItem}
+                required
+              />
+              <button className="btn pa3" onClick={this.saveClick}>
+                Add Task
               </button>
             </div>
 
-            <input
-              onChange={event => {
-                this.setState({ currentItem: event.target.value });
-              }}
-              className="w-70"
-              placeholder="Type and press enter to save"
-              type="text"
-              value={this.state.currentItem}
-            />
+            <ul className="w-70 tl center pa3">
+              {this.state.notes.map(note => (
+                <li className="list pt3">
+                  {note}
+                  <button onClick={this.deleteNote}>Delete</button>
+                </li>
+              ))}
+            </ul>
           </div>
-          <ul className="w-50 center">
-            {!this.state.currentItem ? (
-              <li className="list">Please type to save note</li>
-            ) : (
-              <li className="list">
-                {this.state.currentItem}
-                <button
-                  onClick={() => {
-                    var joined = this.state.notes.concat(
-                      this.state.currentItem
-                    );
-                    this.setState(
-                      {
-                        notes: joined
-                      },
-                      () => this.setState({ currentItem: "" })
-                    );
-                    console.log(`Current item is: ${this.state.currentItem}`);
-                  }}
-                >
-                  Save
-                </button>
-              </li>
-            )}
-
-            {this.state.notes.map(note => (
-              <li className="list">
-                {note}
-                <button onClick={() => alert("Delete")}>Delete</button>
-              </li>
-            ))}
-          </ul>
         </div>
       </main>
     );
