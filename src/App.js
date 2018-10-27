b3a7794fa0ac0e08590ce148f13f66e143b157cf
thechocolateman <hakeemjgreen@gmail.com>
2018-10-27 11:45:29 -0400

    Added Delete functionality and fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 // import logo from './logo.svg';
-import "./App.css";
+// import "./App.css";
 // import { throws } from "assert";
 // import { join } from "upath";
 import { hot } from "react-hot-loader";
-import { NoteList } from "./components/NoteList";
+import NoteList from "./components/NoteList";
 import { TaskBox } from "./components/TaskBox";
 import axios from "axios";
 
@@ -27,20 +27,19 @@ class App extends Component {
             notes: response.data,
             responseLength: response.data.length
           },
-          console.log(response.data)
+          console.log(this.state.notes)
         )
       )
       .catch(error => console.log(error));
   };
-  componentDidMount() {
+  componentWillMount() {
     this.fetchNotes();
   }
 
   componentDidUpdate() {
-    // alert("Component updated");
-
+    //alert("Component updated");
     //If a new note is saved/deleted update state
-    if (this.state.notes.length !== this.state.responseLength) {
+    if (this.state.notes.length > this.state.responseLength) {
       this.fetchNotes();
     } else {
       console.log("fixed");
@@ -54,10 +53,11 @@ class App extends Component {
 
   //Save button click
   saveClick = () => {
-    var joined = this.state.notes.concat(this.state.currentItem);
+    let notes = this.state.notes.slice();
+    var joined = notes.concat(this.state.currentItem);
     let { currentItem } = this.state;
 
-    //If field isn't empty
+    //If field isn't empty submit
     if (this.state.currentItem.length > 0) {
       this.setState({ notes: joined }, () =>
         this.setState({ currentItem: "" })
@@ -74,8 +74,14 @@ class App extends Component {
       alert("ERROR");
     }
   };
-  deleteNote = () => {
-    alert("Delete");
+  deleteNote = key => {
+    //alert(key);
+    //Get all notes except the ones clicked
+    let newNotes = this.state.notes.filter(note => note.id !== key);
+    axios
+      .delete(`https://5bd22342bded6100135c303c.mockapi.io/notes/${key}`)
+      .then(this.setState({ notes: newNotes }));
+    console.log(this.state.notes);
   };
   inputOnChange = event => {
     this.setState({ currentItem: event.target.value });
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
-export const NoteList = props => (
-  <div>
-    <p className="ma0 pb3">You have the following due: </p>
-    {props.notes.map(note => (
-      <div className="fl w-100" key={note["id"]}>
-        <li className="list w-100 bt b--light-gray pv3-l">
-          <div className="fl mr2 round">
-            <input
-              onClick={props.onClick}
-              type="checkbox"
-              // id={"checkbox-" + note["id"]}
-              id="checkbox"
-            />
-            <label htmlFor="checkbox" />
+export default class NoteList extends Component {
+  constructor(props) {
+    super();
+  }
+  onClick = key => {
+    //console.log(this.props);
+    this.props.onClick(key);
+  };
+  render() {
+    return (
+      <div>
+        <p className="ma0 pb3">You have the following due: </p>
+        {this.props.notes.map(note => (
+          <div className="fl w-100" key={note["id"]}>
+            <li className="list w-100 bt b--light-gray pv3-l">
+              <div className="fl mr2 round">
+                <input
+                  onClick={() => this.onClick(note["id"])}
+                  type="checkbox"
+                  id={"checkbox-" + note["id"]}
+                  //   id="checkbox"
+                />
+                <label htmlFor="checkbox" />
+              </div>
+              {note["Note"]}
+            </li>
           </div>
-          {note["Note"]}
-        </li>
+        ))}
       </div>
-    ))}
-  </div>
-);
+    );
+  }
+}
